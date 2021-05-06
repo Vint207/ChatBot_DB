caf1feeb110d53b489c935588aa9a1223dace411
Admin <Admin@BEST-KOMP>
2021-05-06 20:45:21 +0300

    5

--- ChatBot_DB/DataBase/ArchiveDB.cs
+++ ChatBot_DB/DataBase/ArchiveDB.cs
@@ -3,11 +3,24 @@ using System;
 
 namespace ChatBot_DB
 {
-    public class OrdersDB
+    public class ArchiveDB
     {
 
         public Guid TableId { get; set; }
 
+        public void CreateItem(OrderDB order)
+        {
+            SqlCommand query = new($"INSERT INTO [{TableId}] VALUES" +
+                                   $"('" +
+                                   $"{order.TableId}'," +
+                                   $"{order.OpenDate}," +
+                                   $"{order.CloseDate}," +
+                                   $"{order.Paid}," +
+                                   $"{order.Closed}" +        
+                                   $")");
+
+            QueryDB.ExecuteNonQuery(query);
+        }
 
 
         public void CreateTable(Guid id)
@@ -15,7 +28,7 @@ namespace ChatBot_DB
             TableId = id;
             SqlCommand query = new($"CREATE TABLE [{TableId}]" +
                                    $"(" +
-                                   $"[Name] nvarchar(25) UNIQUE CHECK([Name] != '')," +
+                                   $"OrderID uniqueidentifier," +
                                    $"OpenDate DateTime," +
                                    $"CloseDate DateTime," +
                                    $"Paid bit," +
